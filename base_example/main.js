fef7f718132b91874d2c4260de41e38da49a091e
Leon Ji <mgleon08@gmail.com>
2016-12-08 10:38:43 +0800

    Filtered-Sorted-Results

--- base_example/main.js
+++ base_example/main.js
@@ -239,3 +239,24 @@ var app17 = new Vue({
     ok: true
   }
 })
+
+var app19 = new Vue({
+  el: '#app-19',
+  data: {
+  numbers: [ 1, 2, 3, 4, 5 ]
+  },
+  computed: {
+    evenNumbers: function () {
+      return this.numbers.filter(function (number) {
+        return number % 2 === 0
+      })
+    }
+  },
+  methods: {
+  even: function (numbers) {
+    return numbers.filter(function (number) {
+      return number % 2 === 0
+      })
+    }
+  }
+})
