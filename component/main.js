1375ecd5a0bc6d0db3513718dd0f683e992f49a1
Leon Ji <mgleon08@gmail.com>
2016-12-12 14:24:32 +0800

    [component] for todo list use 'is'

--- component/main.js
+++ component/main.js
@@ -27,3 +27,33 @@ var app2 = new Vue({
     isActive: true,
   }
 })
+
+// =======================================================
+
+Vue.component('todo-item', {
+  template: '\
+    <li>\
+      {{ title }}\
+      <button v-on:click="$emit(\'remove\')">X</button>\
+    </li>\
+  ',
+  props: ['title']
+})
+
+var app3 = new Vue({
+  el: '#todo-list-example',
+  data: {
+    newTodoText: '',
+    todos: [
+      'Do the dishes',
+      'Take out the trash',
+      'Mow the lawn'
+    ]
+  },
+  methods: {
+    addNewTodo: function () {
+      this.todos.push(this.newTodoText)
+      this.newTodoText = ''
+    }
+  }
+})
