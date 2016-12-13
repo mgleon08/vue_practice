05b2ef3e86c78b0b1e33ae342b4918bb51f314de
Leon Ji <mgleon08@gmail.com>
2016-12-13 09:55:52 +0800

    [component] props

--- component/main.js
+++ component/main.js
@@ -91,3 +91,93 @@ Vue.component('simple-counter', {
 var app5 = new Vue({
   el: '#app-5'
 })
+
+// =======================================================
+// prop 是父元件用來傳遞數據的一個自定義屬性。子元件需要顯式地用 props 選項 聲明 「prop」
+// camelCase vs. kebab-case
+// HTML 特性不區分大小寫。當使用非字串模版時，prop的名字形式會從 camelCase 轉為 kebab-case（短橫線隔開）：
+// myMessage(camelCase in JavaScript) -> my-message(kebab-case in HTML)
+
+Vue.component('child', {
+  // 聲明 props
+  props: ['message'],
+  // 就像 data 一樣，prop 可以用在樣板內
+  // 同樣也可以在 vm 實例中像 「this.message」 這樣使用
+  template: '<span>{{ message }}</span>'
+})
+
+Vue.component('child2', {
+  props: ['myMessage'],
+  template: '<span>{{ myMessage }}</span>'
+})
+
+Vue.component('child3', {
+  props: ['someProp'],
+  template: '<span>{{ someProp }}</span>'
+})
+
+// One-Way Data Flow
+// 注意在 JavaScript 中物件和陣列是引用類型，指向同一個記憶體空間，如果 prop 是一個物件或陣列，在子元件內部改變它會影響父元件的狀態。
+Vue.component('child4', {
+  template: '<span>{{ counter }}</span>',
+  props: ['initialCounter'],
+  data: function () {
+    return { counter: this.initialCounter }
+  }
+})
+
+Vue.component('child5', {
+  template: '<span>{{ normalizedSize }}</span>',
+  props: ['size'],
+  computed: {
+    normalizedSize: function () {
+      return this.size.trim().toLowerCase()
+    }
+  }
+})
+
+// Prop Validation
+// prop 是一個物件而不是字串陣列時，它包含驗證要求。如果未指定驗證要求，Vue 會發出警告。
+// type: String Number Boolean Function Object Array
+// type 也可以是一個自定義構造器，使用 instanceof 檢測。
+// 當 prop 驗證失敗了， Vue 將拒絕在子元件上設定此值，如果使用的是開發版本會拋出一條警告。
+
+Vue.component('child6', {
+  template: '<span>1. {{propA}} | 2. {{propB}} | 3. {{propC}} | 4. {{propD}} | 5. {{propE}} | 6. {{propF}}</span>',
+  props: {
+    // 基礎類型檢測 （`null` 意思是任何類型都可以）
+    propA: Number,
+    // 多種類型
+    propB: [String, Number],
+    // 必傳且是字串
+    propC: {
+      type: String,
+      required: true
+    },
+    // 數字，有預設值
+    propD: {
+      type: Number,
+      default: 100
+    },
+    // 陣列／物件的預設值應當由一個工廠函式返回
+    propE: {
+      type: Object,
+      default: function () {
+        return { message: 'hello' }
+      }
+    },
+    // 自定義驗證函式
+    propF: {
+      validator: function (value) {
+        return value > 10
+      }
+    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#parents',
+  data: {
+    parentMsg: "i'm Parents"
+  }
+})
