9b742422a661e9f4719b986efe85ddeaafcbe1dd
Leon Ji <mgleon08@gmail.com>
2016-12-23 14:48:21 +0800

    [advanced] Transitioning Between Components

--- advanced_example/main.js
+++ advanced_example/main.js
@@ -46,3 +46,20 @@ var app4 = new Vue({
     }
   }
 })
+
+// ====================================
+
+var app5 = new Vue({
+  el: '#transition-components-demo',
+  data: {
+    view: 'v-a'
+  },
+  components: {
+    'v-a': {
+      template: '<div>Component A</div>'
+    },
+    'v-b': {
+      template: '<div>Component B</div>'
+    }
+  }
+})
