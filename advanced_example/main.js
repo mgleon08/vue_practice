04751e3dd284f9a7d4ead231ddb6e8a47c8089c0
Leon Ji <mgleon08@gmail.com>
2016-12-23 14:54:13 +0800

    [advanced] List Move Transitions

--- advanced_example/main.js
+++ advanced_example/main.js
@@ -84,3 +84,17 @@ var app6 = new Vue({
     },
   }
 })
+
+// ====================================
+
+var app7 = new Vue({
+  el: '#flip-list-demo',
+  data: {
+    items: [1,2,3,4,5,6,7,8,9]
+  },
+  methods: {
+    shuffle: function () {
+      this.items = _.shuffle(this.items)
+    }
+  }
+})
