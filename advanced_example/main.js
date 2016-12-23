318394c858e3a0f8dc03af25839fc06ecb479692
Leon Ji <mgleon08@gmail.com>
2016-12-23 14:30:22 +0800

    [advanced] Transition

---
+++ advanced_example/main.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+  el: '#example-1',
+  data: {
+    show: true
+  }
+})
+
+var app2 = new Vue({
+  el: '#example-2',
+  data: {
+    show: true
+  }
+})
+
+var app3 = new Vue({
+  el: '#example-3',
+  data: {
+    show: true
+  }
+})
