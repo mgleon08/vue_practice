247cb08b82e76c9a89c501344fc0e87aad7d4c76
Leon Ji <mgleon08@gmail.com>
2016-12-23 14:59:24 +0800

    [advanced] list-complete

--- advanced_example/main.js
+++ advanced_example/main.js
@@ -98,3 +98,27 @@ var app7 = new Vue({
     }
   }
 })
+
+// ====================================
+
+var app8 = new Vue({
+  el: '#list-complete-demo',
+  data: {
+    items: [1,2,3,4,5,6,7,8,9],
+    nextNum: 10
+  },
+  methods: {
+    randomIndex: function () {
+      return Math.floor(Math.random() * this.items.length)
+    },
+    add: function () {
+      this.items.splice(this.randomIndex(), 0, this.nextNum++)
+    },
+    remove: function () {
+      this.items.splice(this.randomIndex(), 1)
+    },
+    shuffle: function () {
+      this.items = _.shuffle(this.items)
+    }
+  }
+})
